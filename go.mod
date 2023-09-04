dad8b1822ff68394ea294ed5288ba01cb17718a9
ucpr <contact@ucpr.dev>
2023-09-05 02:22:22 +0900

    feat: add gogen option

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
-module github.com/ucpr/protoc-gen-go-generate
+module github.com/ucpr/protoc-gen-gogen
 
 go 1.21.0
 
-require github.com/lyft/protoc-gen-star/v2 v2.0.3
+require (
+	github.com/golang/protobuf v1.5.0
+	github.com/lyft/protoc-gen-star/v2 v2.0.3
+	google.golang.org/protobuf v1.31.0
+)
 
 require (
 	github.com/spf13/afero v1.9.5 // indirect
@@ -10,5 +14,4 @@ require (
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,66 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
 	pgs "github.com/lyft/protoc-gen-star/v2"
 	pgsgo "github.com/lyft/protoc-gen-star/v2/lang/go"
+	"github.com/ucpr/protoc-gen-gogen/gogen"
 )
 
 func main() {
 	pgs.Init(
 		pgs.DebugEnv("DEBUG"),
-	).RegisterModule().RegisterPostProcessor(
+	).RegisterModule(
+		New(),
+	).RegisterPostProcessor(
 		pgsgo.GoFmt(),
 	).Render()
+}
+
+type Module struct {
+	*pgs.ModuleBase
+	pgs.DebuggerCommon
+	ctx pgsgo.Context
+}
+
+func New() pgs.Module {
+	return &Module{ModuleBase: &pgs.ModuleBase{}}
+}
+
+func (m *Module) InitContext(c pgs.BuildContext) {
+	m.ModuleBase.InitContext(c)
+	m.ctx = pgsgo.InitContext(c.Parameters())
+}
+
+func (m *Module) Name() string {
+	return "gogen"
+}
+
+func (m *Module) Execute(targets map[string]pgs.File, pkgs map[string]pgs.Package) []pgs.Artifact {
+	for _, t := range targets {
+		// gfname := m.ctx.OutputPath(t).SetExt(".go").String()
+
+		// outdir := m.Parameters().Str("outdir")
+		// filename := gfname
+		// if outdir != "" {
+		// 	filename = filepath.Join(outdir, gfname)
+		// }
+		opts := t.File().Descriptor().GetOptions()
+		rawOpt, err := proto.GetExtension(opts, gogen.E_Generate)
+		if err != nil {
+			panic(err)
+		}
+		opt, ok := rawOpt.(*string)
+		if !ok {
+			panic(fmt.Errorf("unable to convert extension to string"))
+		}
+		fmt.Println(*opt)
+
+		// buf := new(bytes.Buffer)
+		// m.OverwriteGeneratorFile(filename, buf.String())
+	}
+
+	return m.Artifacts()
 }
